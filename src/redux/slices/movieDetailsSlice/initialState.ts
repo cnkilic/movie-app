32f6479ef7164cf92edc8bdb21674ba97c5eb9f7
cnkilic <canklc9598@gmail.com>
2024-12-08 16:52:25 +0300

    add fetch movies details thunk

--- src/redux/slices/movieDetailsSlice/initialState.ts
+++ src/redux/slices/movieDetailsSlice/initialState.ts
@@ -1,14 +1,16 @@
 import { AsyncStatus } from "../../../constants/common";
-import { Movie } from "../../../types/movieDetails";
+import { Movie } from "../../../types/types";
 
 export interface MovieDetailsInitialState {
-  movieDetails: Movie | null;
-  movieDetailsLoading: AsyncStatus;
+  movieDetails: Movie | undefined;
+  movieDetailsStatus: AsyncStatus;
+  movieDetailsError: string | undefined;
 }
 
 const movieDetailsInitialState: MovieDetailsInitialState = {
-  movieDetails: null,
-  movieDetailsLoading: AsyncStatus.Idle,
+  movieDetails: undefined,
+  movieDetailsStatus: AsyncStatus.Idle,
+  movieDetailsError: undefined,
 };
 
 export default movieDetailsInitialState;
--- src/redux/slices/movieDetailsSlice/movieDetailsSlice.ts
+++ src/redux/slices/movieDetailsSlice/movieDetailsSlice.ts
@@ -1,18 +1,41 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
 import movieDetailsInitialState from "./initialState";
+import { movieAppApi } from "../../../api";
+import { AsyncStatus } from "../../../constants/common";
+
+export const fetchMovieByImdbIdThunk = createAsyncThunk(
+  "movieDetail/fetchMovieDetails",
+  async (searchParams: { imdbId: string }) => {
+    const response = await movieAppApi.getMovieDetails({
+      imdbId: searchParams.imdbId,
+    });
+
+    return response;
+  }
+);
 
 export const movieDetailsSlice = createSlice({
   name: "movieDetail",
   initialState: movieDetailsInitialState,
-  reducers: {
-    // Use the PayloadAction type to declare the contents of `action.payload`
-    // incrementByAmount: (state, action: PayloadAction<number>) => {
-    //   state. += action.payload;
-    // },
+  reducers: {},
+
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchMovieByImdbIdThunk.pending, (state) => {
+        state.movieDetailsStatus = AsyncStatus.Loading;
+      })
+      .addCase(fetchMovieByImdbIdThunk.fulfilled, (state, action) => {
+        state.movieDetailsStatus = AsyncStatus.Success;
+        state.movieDetails = action.payload;
+      })
+      .addCase(fetchMovieByImdbIdThunk.rejected, (state, action) => {
+        state.movieDetailsStatus = AsyncStatus.Error;
+        state.movieDetails = undefined;
+        state.movieDetailsError = action.error.message;
+        console.error("Error fetching movies:", action.error.message);
+      });
   },
 });
-
-// export const {incrementByAmount } = moviesSlice.actions;
 
 export default movieDetailsSlice.reducer;
